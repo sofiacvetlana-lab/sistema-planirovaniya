cbdbd013a948db1decccd9ad2613e9ee8542346f
sofiacvetlana-lab <sofiacvetlana@gmail.com>
2026-09-15 14:00:01 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,18 @@
-const CACHE_NAME = "sistema-planirovaniya-v3";
+const CACHE_NAME = "sistema-planirovaniya-v4";
 
-const FILES_TO_CACHE = [
+const APP_FILES = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(APP_FILES);
+    })
   );
 
   self.skipWaiting();
@@ -17,13 +20,13 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
+    caches.keys().then(keys => {
+      return Promise.all(
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
-      )
-    )
+      );
+    })
   );
 
   self.clients.claim();
@@ -31,9 +34,24 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
 
-  event.respondWith(
-    caches.match(event.request)
-      .then(cached => cached || fetch(event.request))
-  );
+  if (event.request.method !== "GET") {
+    return;
+  }
 
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
 });
